df7e2a558da0f49d0bbe02eedbe238c41cbebe20
Alex Nash <Alexr.nash@gmail.com>
2023-01-28 16:15:23 -0500

    set up comments framework in tests

--- Main/tests/Employee.test.js
+++ Main/tests/Employee.test.js
@@ -1,7 +1,33 @@
+const { TestScheduler } = require('jest')
 const employee = require('../lib/Employee.js')
 
+// write a test to see if you can instantiate an Employee instance
 describe('employee', () => {
-    
+    // variable for the new Employee
+    // expect the type of that variable to be an object
     
 }) 
 
+// write a test to see if you can set the name as a constructor argument
+TestScheduler("can set name via constructor arguments", () => {
+    // variable for name 
+    // variable for a new Emploee with that name passed in as an argument
+    // expect the variable.name to be the variable that toy set up for name
+})
+
+// same for id
+
+// same for email
+
+// write a test to see if your getter method is working
+TestScheduler("can get name via getName()", () => {
+    // variable for the test value
+    // variable for the new E,ployee with that test vlue passed in
+    // expect that when you call that method - to be the test value variable
+})
+
+// same for id
+
+// same for email
+
+// same for getting role
--- Main/tests/Engineer.test.js
+++ Main/tests/Engineer.test.js
@@ -1,5 +1,8 @@
 const engineer = require('../lib/Engineer.js')
 
+// test to see if you cn set a github account via constructor
 describe('employee', () => {
     
 }) 
+
+// test your get methods
