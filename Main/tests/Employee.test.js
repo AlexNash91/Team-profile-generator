90a36e21b2e954ef3dcd973c0dff34b07e5737dd
Alex Nash <Alexr.nash@gmail.com>
2023-02-01 18:08:17 -0500

    finished constructor arguments for employee

--- Main/tests/Employee.test.js
+++ Main/tests/Employee.test.js
@@ -5,7 +5,7 @@ const Employee = require('../lib/Employee.js')
 describe('employee', () => {
     It('Should return as an object')
     // variable for the new Employee
-    const emp = new Employee(answers.name, answers.id, answers.email, answers.officeNumber)
+    const emp = new Employee(answers.name, answers.id, answers.email)
     // expect the type of that variable to be an object
     expect(typeOf(emp)).toBe(Object)
 }) 
@@ -13,16 +13,27 @@ describe('employee', () => {
 // write a test to see if you can set the name as a constructor argument
 TestScheduler("can set name via constructor arguments", () => {
     // variable for name 
-    const name = employee.name 
+    const name = "Jack"
     // variable for a new Emploee with that name passed in as an argument
-    const newEmp = new Employee(answers.name, answers.id, answers.email, answers.officeNumber)
+    let newEmp = new Employee(name, answers.id, answers.email)
     // expect the variable.name to be the variable that toy set up for name
     expect(newEmp.name).toMatch(name)
 })
 
 // same for id
+TestScheduler("Can set id via constructor arguments", () => {
+    const id = "123"
+    let newEmp = new Employee(answers.name, id, answers.email)
+    expect(newEmp.id).toMatch(id)
+})
 
 // same for email
+TestScheduler("Can set email via constructor arguments", () => {
+    const email = "Jack@Jack.com"
+    let newEmp = new Employee(answers.name, answers.id, email)
+    expect(newEmp.email).toMatch(email)
+})
+
 
 // write a test to see if your getter method is working
 TestScheduler("can get name via getName()", () => {
