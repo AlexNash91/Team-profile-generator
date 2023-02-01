516434fbf8b19cc365f7d00407c63bd8dfff7f9a
Alex Nash <Alexr.nash@gmail.com>
2023-02-01 17:58:57 -0500

    wrote a little more test code

--- Main/tests/Employee.test.js
+++ Main/tests/Employee.test.js
@@ -1,18 +1,23 @@
 const { TestScheduler } = require('jest')
-const employee = require('../lib/Employee.js')
+const Employee = require('../lib/Employee.js')
 
 // write a test to see if you can instantiate an Employee instance
 describe('employee', () => {
+    It('Should return as an object')
     // variable for the new Employee
+    const emp = new Employee(answers.name, answers.id, answers.email, answers.officeNumber)
     // expect the type of that variable to be an object
-    
+    expect(typeOf(emp)).toBe(Object)
 }) 
 
 // write a test to see if you can set the name as a constructor argument
 TestScheduler("can set name via constructor arguments", () => {
     // variable for name 
+    const name = employee.name 
     // variable for a new Emploee with that name passed in as an argument
+    const newEmp = new Employee(answers.name, answers.id, answers.email, answers.officeNumber)
     // expect the variable.name to be the variable that toy set up for name
+    expect(newEmp.name).toMatch(name)
 })
 
 // same for id
