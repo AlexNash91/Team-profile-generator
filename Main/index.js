a2da27fde14c1e1cec0610ca755cccc182c0ad84
Alex Nash <Alexr.nash@gmail.com>
2023-02-12 18:32:45 -0500

    set up values for models

--- Main/index.js
+++ Main/index.js
@@ -3,7 +3,7 @@ const employee = require('./lib/Employee.js')
 const engineer = require('./lib/Engineer.js')
 const intern = require('./lib/Intern.js')
 const manager = require('./lib/Manager.js')
-const team = require("./dist/team.html")
+// const team = require('./dist/team.html')
 // require inquirer and fs
 const inquire = require('inquirer')
 const fs = require('fs')
